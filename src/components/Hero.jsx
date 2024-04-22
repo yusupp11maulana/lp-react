8c65fd92847efa66510949ba7bd683bd98baf562
Yusup Maulana <yusupp11maulana@gmail.com>
2024-04-23 02:50:30 +0700

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="container mx-auto md:flex pt-10 md:gap-28 xl:gap-56 2xl:gap-96 justify-center hidden">
-        <div className="border border-red-600 my-auto">
+        <div className="my-auto">
           <h1 className="text-white text-xl tracking-widest font-semibold font-viga lg:text-4xl">I'm Yusup Maulana</h1>
           <p className="text-slate-400 text-lg lg:text-xl mb-3">Full Stack Developer</p>
           <div className="flex gap-4">
@@ -62,24 +62,11 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className="border border-red-600 ">
+        <div>
           <img src={Logo} alt="" className="h-60 object-contain hidden md:block lg:h-96" />
         </div>
       </div>
     </section>
-    // <section className="bg-zinc-800">
-    //   <div className="container mx-auto pt-16 px-4 flex flex-col justify-center md:flex-row md:items-center md:justify-start md:space-x-16 xl:space-x-28 2xl:space-x-52">
-    //     <img src="../assets/asset-no-bg.png" alt="" className="h-56 object-contain mb-6 md:hidden" />
-    //     <div className="text-center mb-8 md:text-left md:mb-0">
-    //       <h1 className="text-white text-xl tracking-widest font-semibold font-viga lg:text-4xl">I'm Yusup Maulana</h1>
-    //       <p className="text-slate-400 text-lg lg:text-xl mb-3">Full Stack Developer</p>
-    //       <a href="#about" className="border rounded-full py-2 px-6 text-xs text-sky-500 border-sky-500 hover:bg-sky-500 hover:text-white transition duration-200 cursor-pointer focus:ring-4 lg:text-sm md:text-sm">
-    //         About Me
-    //       </a>
-    //     </div>
-    //     <img src="../assets/asset-no-bg.png" alt="" className="h-60 object-contain hidden md:block lg:h-96" />
-    //   </div>
-    // </section>
   );
 };
 
